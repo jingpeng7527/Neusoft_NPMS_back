ebeeb35b93da7eb92c38dfc97932c03c6ae96128
Abigail8n21 <937206610@qq.com>
2021-07-21 01:33:23 +0800

    gateway-1.1 little change

--- micro-oauth2-gateway/src/main/java/com/macro/cloud/filter/IgnoreUrlsRemoveJwtFilter.java
+++ micro-oauth2-gateway/src/main/java/com/macro/cloud/filter/IgnoreUrlsRemoveJwtFilter.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class IgnoreUrlsRemoveJwtFilter implements WebFilter {
     @Autowired
     private IgnoreUrlsConfig ignoreUrlsConfig;
-    @Override
+
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         URI uri = request.getURI();

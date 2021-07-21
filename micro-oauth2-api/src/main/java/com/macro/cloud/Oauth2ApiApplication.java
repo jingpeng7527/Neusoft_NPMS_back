1412c6498e694192700c5a49c2ab7ca38c577b82
yky <1733596081@qq.com>
2021-07-21 23:11:53 +0800

    傻逼跨域跨不过去

--- micro-oauth2-api/src/main/java/com/macro/cloud/Oauth2ApiApplication.java
+++ micro-oauth2-api/src/main/java/com/macro/cloud/Oauth2ApiApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//@CrossOrigin
 @EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.macro.cloud.mapper")
